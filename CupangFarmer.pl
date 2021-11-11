fd79220315f80f6abf919ddfe3f2d41f268f69f3
Bryan Bernigen <bryanbernigen@gmail.com>
2021-11-11 22:24:43 +0700

    Added time mechanism

--- CupangFarmer.pl
+++ CupangFarmer.pl
@@ -8,161 +8,161 @@
 :- dynamic(playerpos/2).
 :- dynamic(job/1).
 
-map(0,0,#).
-map(0,1,#).
-map(0,2,#).
-map(0,3,#).
-map(0,4,#).
-map(0,5,#).
-map(0,6,#).
-map(0,7,#).
-map(0,8,#).
-map(0,9,#).
-map(0,10,#).
-map(0,11,#).
+loc(0,0,#).
+loc(0,1,#).
+loc(0,2,#).
+loc(0,3,#).
+loc(0,4,#).
+loc(0,5,#).
+loc(0,6,#).
+loc(0,7,#).
+loc(0,8,#).
+loc(0,9,#).
+loc(0,10,#).
+loc(0,11,#).
 
-map(1,0,#).
-map(1,1,o).
-map(1,2,o).
-map(1,3,o).
-map(1,4,o).
-map(1,5,-).
-map(1,6,-).
-map(1,7,-).
-map(1,8,-).
-map(1,9,-).
-map(1,10,-).
-map(1,11,#).
+loc(1,0,#).
+loc(1,1,o).
+loc(1,2,o).
+loc(1,3,o).
+loc(1,4,o).
+loc(1,5,-).
+loc(1,6,-).
+loc(1,7,-).
+loc(1,8,-).
+loc(1,9,-).
+loc(1,10,-).
+loc(1,11,#).
 
-map(2,0,#).
-map(2,1,o).
-map(2,2,o).
-map(2,3,o).
-map(2,4,-).
-map(2,5,-).
-map(2,6,-).
-map(2,7,-).
-map(2,8,-).
-map(2,9,-).
-map(2,10,-).
-map(2,11,#).
+loc(2,0,#).
+loc(2,1,o).
+loc(2,2,o).
+loc(2,3,o).
+loc(2,4,-).
+loc(2,5,-).
+loc(2,6,-).
+loc(2,7,-).
+loc(2,8,-).
+loc(2,9,-).
+loc(2,10,-).
+loc(2,11,#).
 
-map(3,0,#).
-map(3,1,o).
-map(3,2,o).
-map(3,3,-).
-map(3,4,-).
-map(3,5,-).
-map(3,6,-).
-map(3,7,-).
-map(3,8,-).
-map(3,9,-).
-map(3,10,-).
-map(3,11,#).
+loc(3,0,#).
+loc(3,1,o).
+loc(3,2,o).
+loc(3,3,-).
+loc(3,4,-).
+loc(3,5,-).
+loc(3,6,-).
+loc(3,7,-).
+loc(3,8,-).
+loc(3,9,-).
+loc(3,10,-).
+loc(3,11,#).
 
-map(4,0,#).
-map(4,1,o).
-map(4,2,o).
-map(4,3,-).
-map(4,4,-).
-map(4,5,-).
-map(4,6,-).
-map(4,7,-).
-map(4,8,-).
-map(4,9,-).
-map(4,10,-).
-map(4,11,#).
+loc(4,0,#).
+loc(4,1,o).
+loc(4,2,o).
+loc(4,3,-).
+loc(4,4,-).
+loc(4,5,-).
+loc(4,6,-).
+loc(4,7,-).
+loc(4,8,-).
+loc(4,9,-).
+loc(4,10,-).
+loc(4,11,#).
 
-map(5,0,#).
-map(5,1,-).
-map(5,2,-).
-map(5,3,-).
-map(5,4,-).
-map(5,5,-).
-map(5,6,-).
-map(5,7,-).
-map(5,8,-).
-map(5,9,-).
-map(5,10,-).
-map(5,11,#).
+loc(5,0,#).
+loc(5,1,-).
+loc(5,2,-).
+loc(5,3,-).
+loc(5,4,-).
+loc(5,5,-).
+loc(5,6,-).
+loc(5,7,-).
+loc(5,8,-).
+loc(5,9,-).
+loc(5,10,-).
+loc(5,11,#).
 
-map(6,0,#).
-map(6,1,-).
-map(6,2,-).
-map(6,3,-).
-map(6,4,-).
-map(6,5,-).
-map(6,6,-).
-map(6,7,-).
-map(6,8,r).
-map(6,9,-).
-map(6,10,-).
-map(6,11,#).
+loc(6,0,#).
+loc(6,1,-).
+loc(6,2,-).
+loc(6,3,-).
+loc(6,4,-).
+loc(6,5,-).
+loc(6,6,-).
+loc(6,7,-).
+loc(6,8,r).
+loc(6,9,-).
+loc(6,10,-).
+loc(6,11,#).
 
-map(7,0,#).
-map(7,1,-).
-map(7,2,-).
-map(7,3,-).
-map(7,4,-).
-map(7,5,-).
-map(7,6,-).
-map(7,7,-).
-map(7,8,-).
-map(7,9,-).
-map(7,10,-).
-map(7,11,#).
+loc(7,0,#).
+loc(7,1,-).
+loc(7,2,-).
+loc(7,3,-).
+loc(7,4,-).
+loc(7,5,-).
+loc(7,6,-).
+loc(7,7,-).
+loc(7,8,-).
+loc(7,9,-).
+loc(7,10,-).
+loc(7,11,#).
 
-map(8,0,#).
-map(8,1,-).
-map(8,2,-).
-map(8,3,-).
-map(8,4,-).
-map(8,5,-).
-map(8,6,-).
-map(8,7,h).
-map(8,8,-).
-map(8,9,-).
-map(8,10,-).
-map(8,11,#).
+loc(8,0,#).
+loc(8,1,-).
+loc(8,2,-).
+loc(8,3,-).
+loc(8,4,-).
+loc(8,5,-).
+loc(8,6,-).
+loc(8,7,h).
+loc(8,8,-).
+loc(8,9,-).
+loc(8,10,-).
+loc(8,11,#).
 
-map(9,0,#).
-map(9,1,-).
-map(9,2,-).
-map(9,3,m).
-map(9,4,-).
-map(9,5,-).
-map(9,6,-).
-map(9,7,-).
-map(9,8,-).
-map(9,9,-).
-map(9,10,-).
-map(9,11,#).
+loc(9,0,#).
+loc(9,1,-).
+loc(9,2,-).
+loc(9,3,m).
+loc(9,4,-).
+loc(9,5,-).
+loc(9,6,-).
+loc(9,7,-).
+loc(9,8,-).
+loc(9,9,-).
+loc(9,10,-).
+loc(9,11,#).
 
-map(10,0,#).
-map(10,1,-).
-map(10,2,-).
-map(10,3,-).
-map(10,4,-).
-map(10,5,-).
-map(10,6,-).
-map(10,7,-).
-map(10,8,-).
-map(10,9,-).
-map(10,10,-).
-map(10,11,#).
+loc(10,0,#).
+loc(10,1,-).
+loc(10,2,-).
+loc(10,3,-).
+loc(10,4,-).
+loc(10,5,-).
+loc(10,6,-).
+loc(10,7,-).
+loc(10,8,-).
+loc(10,9,-).
+loc(10,10,-).
+loc(10,11,#).
 
-map(11,0,#).
-map(11,1,#).
-map(11,2,#).
-map(11,3,#).
-map(11,4,#).
-map(11,5,#).
-map(11,6,#).
-map(11,7,#).
-map(11,8,#).
-map(11,9,#).
-map(11,10,#).
-map(11,11,#).
+loc(11,0,#).
+loc(11,1,#).
+loc(11,2,#).
+loc(11,3,#).
+loc(11,4,#).
+loc(11,5,#).
+loc(11,6,#).
+loc(11,7,#).
+loc(11,8,#).
+loc(11,9,#).
+loc(11,10,#).
+loc(11,11,#).
 
 startGame :-
 	asserta(uang(100)),
@@ -170,7 +170,7 @@ startGame :-
 	asserta(farmexp(0)),
 	asserta(fishexp(0)),
 	asserta(ranchexp(0)),
-	asserta(time(0)),
+	asserta(time(23)),
 	asserta(day(1)),
 	asserta(playerpos(8,7)),
 	promptStart.
@@ -186,7 +186,7 @@ promptMenu :-
 	write('%                       Menu                     %'),nl,
 	write('%  1. start   : Mulai pertualanganmu             %'),nl,
 	write('%  2. menu    : Menampilkan Menu                 %'),nl,
-	write('%  3. showmap : Menampilkan Map                  %'),nl,
+	write('%  3. showmap : Menampilkan map                  %'),nl,
 	write('%  4. status  : Menampilkan kondisimu terkini    %'),nl,
 	write('%  5. w       : Gerak ke utara 1 langkah         %'),nl,
 	write('%  6. a       : Gerak ke barat 1 langkah         %'),nl,
@@ -195,16 +195,21 @@ promptMenu :-
 	write('%  9. help    : Menampilkan peraturan game       %'),nl,
 	write('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%').
 
-showmap:- printmap(0,0),!.
+addTime(X):- time(T), NextT is T+X, NextT<24, retractall(time(_)), asserta(time(NextT)),!.
+addTime(X):- time(T), NextT is mod(T+X,24), day(CurrDay), NextDay is CurrDay+1, retractall(day(_)), asserta(day(NextDay)),retractall(time(_)), asserta(time(NextT)),!.
+
+time:- day(D), time(T), write('Day '),write(D),write(' '),write(T),write(':00'),!.
+
+map:- printmap(0,0),!.
 printmap(12,0):- !.
 printmap(Y,12):- nl, NextY is Y+1, printmap(NextY,0),!.
 printmap(Y,X):- playerpos(Yp,Xp),Yp=:=Y,Xp=:=X,write('P'), NextX is X+1, printmap(Y,NextX),!.
-printmap(Y,X):- map(Y,X,A), A==r, write('R'), NextX is X+1, printmap(Y,NextX),!.
-printmap(Y,X):- map(Y,X,A), A==h, write('H'), NextX is X+1, printmap(Y,NextX),!.
-printmap(Y,X):- map(Y,X,A), A==m, write('M'), NextX is X+1, printmap(Y,NextX),!.
-printmap(Y,X):- map(Y,X,A), write(A), NextX is X+1, printmap(Y,NextX),!.
+printmap(Y,X):- loc(Y,X,A), A==r, write('R'), NextX is X+1, printmap(Y,NextX),!.
+printmap(Y,X):- loc(Y,X,A), A==h, write('H'), NextX is X+1, printmap(Y,NextX),!.
+printmap(Y,X):- loc(Y,X,A), A==m, write('M'), NextX is X+1, printmap(Y,NextX),!.
+printmap(Y,X):- loc(Y,X,A), write(A), NextX is X+1, printmap(Y,NextX),!.
 
-w:-playerpos(Y,X),NextY is Y-1, NextY>0, \+ (map(NextY,X,A),A=='o'), retractall(playerpos(_,_)), asserta(playerpos(NextY,X)),!.
-a:-playerpos(Y,X),NextX is X-1, NextX>0, \+ (map(Y,NextX,A),A=='o'), retractall(playerpos(_,_)), asserta(playerpos(Y,NextX)),!.
-s:-playerpos(Y,X),NextY is Y+1, NextY<11, \+ (map(NextY,X,A),A=='o'), retractall(playerpos(_,_)), asserta(playerpos(NextY,X)),!.
-d:-playerpos(Y,X),NextX is X+1, NextX<11, \+ (map(Y,NextX,A),A=='o'), retractall(playerpos(_,_)), asserta(playerpos(Y,NextX)),!.
+w:-playerpos(Y,X),NextY is Y-1, NextY>0, \+ (loc(NextY,X,A),A=='o'), retractall(playerpos(_,_)), asserta(playerpos(NextY,X)),addTime(1),!.
+a:-playerpos(Y,X),NextX is X-1, NextX>0, \+ (loc(Y,NextX,A),A=='o'), retractall(playerpos(_,_)), asserta(playerpos(Y,NextX)),addTime(1),!.
+s:-playerpos(Y,X),NextY is Y+1, NextY<11, \+ (loc(NextY,X,A),A=='o'), retractall(playerpos(_,_)), asserta(playerpos(NextY,X)),addTime(1),!.
+d:-playerpos(Y,X),NextX is X+1, NextX<11, \+ (loc(Y,NextX,A),A=='o'), retractall(playerpos(_,_)), asserta(playerpos(Y,NextX)),addTime(1),!.
